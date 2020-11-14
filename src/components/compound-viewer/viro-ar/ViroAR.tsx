e7a07dad77d9fcc6e57d0ee8edbf35336c819e6e
bch0ng <bchong@uw.edu>
2020-11-14 15:05:05 -0800

    wip

--- src/components/compound-viewer/viro-ar/ViroAR.tsx
+++ src/components/compound-viewer/viro-ar/ViroAR.tsx
@@ -6,19 +6,103 @@ import {
     ViroConstants,
     ViroARSceneNavigator,
     ViroMaterials,
-    ViroBox
+    ViroAmbientLight,
+    ViroOrbitCamera,
+    ViroSpotLight,
+    ViroOmniLight
 } from 'react-viro';
-import { View, Text, ActivityIndicator } from 'react-native';
+import {
+    View,
+    Text,
+    ActivityIndicator,
+    StyleSheet,
+    SafeAreaView,
+    TouchableHighlight
+} from 'react-native';
+import * as atoms from '../../../assets/atoms.json';
 
-ViroMaterials.createMaterials({
-    grid: {
-        diffuseTexture: require('../../../../assets/grid_bg.jpg')
+interface Material {
+    roughness?: number;
+    metalness?: number;
+    diffuseColor: string;
+    lightingModel?: string;
+}
+function createMaterials(atomNums: number[]): void {
+    const materials: Record<string, Material> = {};
+    for (const atomNum of atomNums) {
+        // @ts-ignore
+        // Due to the structure of the JSON being like a map,
+        // we ignore this TypeScript warning/error.
+        const atom = atoms[atomNum.toString(10)];
+        if (!(atom.name.toLowerCase() in materials)) {
+            materials[atom.name.toLowerCase()] = {
+                roughness: 0.0,
+                metalness: 0.3,
+                diffuseColor: `#${atom.hexColor}`,
+                lightingModel: 'PBR'
+            };
+        }
     }
-});
+    ViroMaterials.createMaterials(materials);
+}
 
-function Scene(): JSX.Element {
+function Lighting(): JSX.Element {
+    return (
+        <>
+            <ViroOmniLight
+                intensity={350}
+                position={[-10, -10, 1]}
+                color={'#FFFFFF'}
+                attenuationStartDistance={20}
+                attenuationEndDistance={30}
+            />
+            <ViroOmniLight
+                intensity={350}
+                position={[10, -10, 1]}
+                color={'#FFFFFF'}
+                attenuationStartDistance={20}
+                attenuationEndDistance={30}
+            />
+            <ViroOmniLight
+                intensity={350}
+                position={[-10, 10, 1]}
+                color={'#FFFFFF'}
+                attenuationStartDistance={20}
+                attenuationEndDistance={30}
+            />
+            <ViroOmniLight
+                intensity={350}
+                position={[10, 10, 1]}
+                color={'#FFFFFF'}
+                attenuationStartDistance={20}
+                attenuationEndDistance={30}
+            />
+            <ViroOmniLight
+                intensity={350}
+                position={[10, 10, -1]}
+                color={'#FFFFFF'}
+                attenuationStartDistance={20}
+                attenuationEndDistance={30}
+            />
+        </>
+    );
+}
+
+interface Atom {
+    id: number;
+    symbol: string;
+    name: string;
+    category: string;
+    mass: number;
+    color: string;
+}
+function Scene(props: any): JSX.Element {
+    const atomNums = [1, 1, 8];
     const [isTracking, setIsTracking] = React.useState(false);
-    const [hasTouched, setHasTouched] = React.useState(false);
+
+    React.useEffect((): void => {
+        createMaterials(atomNums);
+    }, []);
 
     function onInitialized(state: any, reason: any): void {
         if (state == ViroConstants.TRACKING_NORMAL) {
@@ -28,33 +112,44 @@ function Scene(): JSX.Element {
         }
     }
 
-    function onTouch(): void {
-        setHasTouched(!hasTouched);
-    }
-
     return (
         <ViroARScene onTrackingUpdated={onInitialized}>
-            {hasTouched && (
-                <ViroText
-                    text={isTracking ? 'Hello world' : 'Bye world'}
-                    position={[0, 0, -1]}
-                    style={{ color: 'black', fontSize: 30 }}
-                />
+            <Lighting />
+            {atomNums.map(
+                (atomNum, idx): JSX.Element => {
+                    // @ts-ignore
+                    const atom = atoms[atomNum.toString(10)];
+                    return (
+                        <ViroSphere
+                            key={`atom_${idx}_${atomNum}`}
+                            heightSegmentCount={20}
+                            widthSegmentCount={20}
+                            radius={0.01}
+                            position={[0, -0.25 * (idx * 0.1), -0.25]}
+                            scale={[1, 1, 1]}
+                            materials={[atom.name.toLowerCase()]}
+                            onClick={(): void => {
+                                props.arSceneNavigator.viroAppProps.setClickedAtom(
+                                    {
+                                        id: atomNum,
+                                        symbol: atom.symbol,
+                                        name: atom.name,
+                                        category: atom.category,
+                                        mass: atom.mass,
+                                        color: atom.hexColor
+                                    }
+                                );
+                            }}
+                        />
+                    );
+                }
             )}
-            <ViroSphere
-                heightSegmentCount={20}
-                widthSegmentCount={20}
-                radius={0.01}
-                position={[0, -0.25, -0.25]}
-                scale={[1, 1, 1]}
-                materials={['grid']}
-                onClick={onTouch}
-            />
         </ViroARScene>
     );
 }
 
 export function ViroAR(): JSX.Element {
+    const [clickedAtom, setClickedAtom] = React.useState<Atom | null>(null);
     return (
         <View style={{ flex: 1, width: '100%', height: '100%' }}>
             <ViroARSceneNavigator
@@ -63,11 +158,54 @@ export function ViroAR(): JSX.Element {
                     scene: Scene
                 }}
                 viroAppProps={{
-                    displayObject: true
+                    displayObject: true,
+                    setClickedAtom: setClickedAtom
                 }}
                 autofocus={true}
                 worldAlignment={'Gravity'}
             />
+            {clickedAtom && (
+                <SafeAreaView
+                    style={{
+                        position: 'absolute',
+                        bottom: 0,
+                        width: '100%',
+                        backgroundColor: 'black'
+                    }}
+                >
+                    <View
+                        style={{
+                            display: 'flex',
+                            flexDirection: 'row',
+                            justifyContent: 'space-between',
+                            paddingTop: 10,
+                            paddingRight: 10,
+                            paddingBottom: 50,
+                            paddingLeft: 10
+                        }}
+                    >
+                        <Text
+                            style={{
+                                flexDirection: 'column',
+                                color: `#${clickedAtom.color}`
+                            }}
+                        >
+                            {clickedAtom.id}
+                            {clickedAtom.symbol}
+                            {clickedAtom.name}
+                            {clickedAtom.mass}
+                            {clickedAtom.category}
+                        </Text>
+                        <TouchableHighlight
+                            onPress={(): void => {
+                                setClickedAtom(null);
+                            }}
+                        >
+                            <Text style={{ color: 'white' }}>Close</Text>
+                        </TouchableHighlight>
+                    </View>
+                </SafeAreaView>
+            )}
         </View>
     );
 }
